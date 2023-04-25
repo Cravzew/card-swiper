83bce894fa39a7abda788199df5cb4d0303b93d9
Cravzew <korolnyy2@mail.ru>
2023-04-25 18:52:01 +0300

    bug fixed

--- src/components/Lists/Lists.tsx
+++ src/components/Lists/Lists.tsx
@@ -7,13 +7,22 @@ function Lists() {
 
     const filters = useAppSelector(state => state.photo.filters)
 
+    // Add layout to list and delete
+
     return (
-        <div>
+        <div style={{
+            minHeight: 'calc(100vh - 100px)'
+        }}>
             <h1 style={{
                 textAlign: 'center'
             }}>Liked Image</h1>
-            <Flex wrap={'wrap'} align={'center'}>
-                {filters.map((item) => <ItemCard key={item.id} url={item.url}/>)}
+            <Flex wrap={'wrap'} align={'center'} justify={'center'}>
+                {filters.length === 0 ? <p style={{
+                        marginTop: '30px',
+                        fontSize: '22px'
+                    }}>Lists is empty</p> :
+                    filters.map((item) => <ItemCard key={item.id} url={item.url}/>)
+                }
             </Flex>
         </div>
     );
